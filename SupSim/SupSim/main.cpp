cdd01a01560311b09bcf074a49e83c125d7594d8
Katie <katie.robinson@utah.edu>
2019-04-08 21:27:29 -0600

    Refactored into simulation method, added comments in main, ands added simple input validation

--- SupSim/SupSim/main.cpp
+++ SupSim/SupSim/main.cpp
@@ -18,7 +18,9 @@
 
 using namespace std;
 
-RandDouble randomizer;
+static int numTellers = 6;
+static double numMinutes = 720;
+static RandDouble randomizer;
 
 typedef struct {
     double numDept;
@@ -30,26 +32,16 @@ bool compare(Customer& c1, Customer& c2) {
     return (c1.getPickTime() < c2.getPickTime());
 }
 
-int main(int argc, const char * argv[]) {
-    
-    double avgArrivalRt = atof(argv[1]);
-    double maxServTime = atof(argv[2]);
-    unsigned int seed = atoi(argv[3]);
-    int numTellers = 6;
-    double numMinutes = 720;
+void runSimulation(double numMinutes, queue<Customer> q, vector<Customer> l, double maxServTime, unsigned int seed, double avgArrivalRt, stat &s) {
     bool isList, isQueue;
-    queue<Customer> q;
-    vector<Customer> l;
-    vector<Customer>::iterator list;
     Event event(numMinutes);
-    stat s = {0.0, 0.0, 0.0};
-
+    vector<Customer>::iterator list;
     double time = 0;
     while (time <= numMinutes) {
         isList = (!l.empty());
         isQueue = (!q.empty());
         time = event.checkEvent(numMinutes, isList, isQueue);
-
+        
         if (event.getNextArr() == time) {
             Customer c(event, time, randomizer.fRand(.1, maxServTime, seed));
             l.push_back(c);
@@ -57,13 +49,13 @@ int main(int argc, const char * argv[]) {
             list = min_element(l.begin(), l.end(), compare);
             if ((*list).getPickTime() < event.getNextCheckout()) event.setNextCheckout((*list).getPickTime());
         }
-
+        
         if (event.getNextCheckout() == time) {
             list = min_element(l.begin(), l.end(), compare);
             if (q.empty()) event.setNextDept(event.getNextCheckout(), rand() % 6 + 1);
             q.push(*list);
             l.erase(list);
-
+            
             if (!l.empty()) {
                 list = min_element(l.begin(), l.end(), compare);
                 event.setNextCheckout((*list).getPickTime());
@@ -71,9 +63,8 @@ int main(int argc, const char * argv[]) {
                 event.setNextCheckout(numMinutes + 1);
             }
         }
-
+        
         if (event.getNextDept() == time) {
-            //cout << "depart" << endl;
             Customer temp = q.front();
             q.pop();
             s.numDept++;
@@ -87,12 +78,80 @@ int main(int argc, const char * argv[]) {
             }
         }
     }
+}
 
-    double avg_wait = s.totWait / s.numDept;
-    double avg_serv = s.totServ / s.numDept;
+int main(int argc, const char * argv[]) {
 
-    cout << "Average Wait time:" << avg_wait << endl;
-    cout << "Serve time :" << avg_serv << endl;
-    cout << "Customer :" << s.numDept << endl;
+    // Reading user input 
+    double avgArrivalRt = atof(argv[1]);
+    double maxServTime = atof(argv[2]);
+    unsigned int seed = atoi(argv[3]);
+
+    // Simple input validation
+    if (avgArrivalRt <= 0) {
+        cout << "Average arrival rate must be greater than 0.\n";
+    }
+    if (maxServTime <= 0) {
+        cout << "Max service time must be greater than 0.\n";
+    }
+    if (seed <= 0) {
+        cout << "Random seed must be greater than 0.\n";
+    }
+
+    // Creating data structures
+    queue<Customer> q;
+    vector<Customer> l;
+
+    // Running simulation
+    stat s = {0.0, 0.0, 0.0};
+    runSimulation(numMinutes, q, l, maxServTime, seed, avgArrivalRt, s);
+    
+//    double time = 0;
+//    while (time <= numMinutes) {
+//        isList = (!l.empty());
+//        isQueue = (!q.empty());
+//        time = event.checkEvent(numMinutes, isList, isQueue);
+//
+//        if (event.getNextArr() == time) {
+//            Customer c(event, time, randomizer.fRand(.1, maxServTime, seed));
+//            l.push_back(c);
+//            event.setNextArr(event.getNextArr(), avgArrivalRt);
+//            list = min_element(l.begin(), l.end(), compare);
+//            if ((*list).getPickTime() < event.getNextCheckout()) event.setNextCheckout((*list).getPickTime());
+//        }
+//
+//        if (event.getNextCheckout() == time) {
+//            list = min_element(l.begin(), l.end(), compare);
+//            if (q.empty()) event.setNextDept(event.getNextCheckout(), rand() % 6 + 1);
+//            q.push(*list);
+//            l.erase(list);
+//
+//            if (!l.empty()) {
+//                list = min_element(l.begin(), l.end(), compare);
+//                event.setNextCheckout((*list).getPickTime());
+//            } else {
+//                event.setNextCheckout(numMinutes + 1);
+//            }
+//        }
+//
+//        if (event.getNextDept() == time) {
+//            Customer temp = q.front();
+//            q.pop();
+//            s.numDept++;
+//            s.totWait += temp.getWaitTime();
+//            s.totServ += (event.getNextDept() - (temp.getPickTime() + temp.getWaitTime()));
+//            if (!q.empty()) {
+//                q.front().setWaitTime(event.getNextDept() - q.front().getPickTime());
+//                event.setNextDept(event.getNextDept(), numTellers);
+//            } else {
+//                event.setNextDept(numMinutes, 1);
+//            }
+//        }
+//    }
+
+    // Print simulation results
+    cout << "Average wait time per customer: " << s.totWait / s.numDept << endl;
+    cout << "Number of customers: " << s.numDept << endl;
+    
     return 0;
 }
